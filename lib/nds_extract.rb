ee2a83b389dddd8820e66bd51a039c22f64c6763
Anna Dybas <a.m.dybas@gmail.com>
2019-12-03 07:17:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,8 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  directors_name = []
+  directors_name << source[0][:name]
 end
 
 def total_gross(source)
@@ -39,5 +41,3 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 end
-
-
